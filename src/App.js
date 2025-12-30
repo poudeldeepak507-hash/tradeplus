8924a7e52be2d1891fc969443d1c3c5758138e5c
Deepak Poudel <poudeldeepak507@gmail.com>
2025-12-31 02:41:12 +0545

    clean package.json for ender deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Tabs, Tab, Box, Typography } from "@mui/material";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Tabs,
+  Tab,
+  Box,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import Deposit from "./pages/Deposit";
 import Balance from "./pages/Balance";
@@ -12,12 +33,30 @@ import News from "./pages/News";
 
 import logoA from "./pages/logoA.png";
 
-export default function App() {
-  const [value, setValue] = React.useState(0);
+const navItems = [
+  { label: "Balance", path: "/" },
+  { label: "Trade", path: "/trade" },
+  { label: "Profit/Loss", path: "/profit-loss" },
+  { label: "Transactions", path: "/transactions" },
+  { label: "Withdraw", path: "/withdraw" },
+  { label: "Top News", path: "/news" },
+];
+
+function Navigation() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const location = useLocation();
+  const currentTab = navItems.findIndex(
+    (item) => item.path === location.pathname
+  );
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
   return (
-    <Router>
+    <>
       <AppBar position="static">
         <Toolbar>
+          {/* Logo */}
           <Box
             component={Link}
             to="/"
@@ -26,46 +65,75 @@ export default function App() {
               alignItems: "center",
               textDecoration: "none",
               color: "inherit",
-              mr: 4
+              mr: 3,
             }}
           >
-            <Box
-              component="img"
-              src={logoA}
-              alt="TradePlus Logo"
-              sx={{ height: 36, mr: 1 }}
-            />
+            <Box component="img" src={logoA} alt="Logo" sx={{ height: 36, mr: 1 }} />
             <Typography
               variant="h6"
               sx={{
-                fontWeight: 400,
+                fontWeight: 500,
                 letterSpacing: "0.15em",
                 textTransform: "uppercase",
-                fontFamily: "'Inter', 'Roboto', sans-serif",
               }}
             >
               Trading Plus
             </Typography>
-
-
           </Box>
 
-          <Tabs
-            value={value}
-            onChange={(e, val) => setValue(val)}
-            textColor="inherit"
-            indicatorColor="secondary"
-          >
-            <Tab label="Balance" component={Link} to="/" />
-            <Tab label="Trade" component={Link} to="/trade" />
-            <Tab label="Profit/Loss" component={Link} to="/profit-loss" />
-            <Tab label="Transactions" component={Link} to="/transactions" />
-            <Tab label="Withdraw" component={Link} to="/withdraw" />
-            <Tab label="Top News" component={Link} to="/news" />
-
-          </Tabs>
+          {/* Mobile Menu */}
+          {isMobile ? (
+            <>
+              <Box sx={{ flexGrow: 1 }} />
+              <IconButton color="inherit" onClick={() => setDrawerOpen(true)}>
+                <MenuIcon />
+              </IconButton>
+            </>
+          ) : (
+            <Tabs
+              value={currentTab === -1 ? false : currentTab}
+              textColor="inherit"
+              indicatorColor="secondary"
+            >
+              {navItems.map((item) => (
+                <Tab
+                  key={item.path}
+                  label={item.label}
+                  component={Link}
+                  to={item.path}
+                />
+              ))}
+            </Tabs>
+          )}
         </Toolbar>
       </AppBar>
+
+      {/* Mobile Drawer */}
+      <Drawer anchor="left" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <Box sx={{ width: 250 }}>
+          <List>
+            {navItems.map((item) => (
+              <ListItemButton
+                key={item.path}
+                component={Link}
+                to={item.path}
+                onClick={() => setDrawerOpen(false)}
+              >
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Navigation />
+
       <Box p={2}>
         <Routes>
           <Route path="/" element={<Balance />} />
@@ -74,9 +142,8 @@ export default function App() {
           <Route path="/transactions" element={<Transactions />} />
           <Route path="/withdraw" element={<Withdraw />} />
           <Route path="/news" element={<News />} />
-
         </Routes>
       </Box>
     </Router>
-  )
+  );
 }
